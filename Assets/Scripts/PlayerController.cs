1703c71b5c6bd29fc7a2ad0fd6204b627746e084
Isaac Yung Lopes <isaaczoi@gmail.com>
2020-03-04 20:35:16 -0300

    Add script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Ola mundo");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log("Ola mundo");
+    }
+}
